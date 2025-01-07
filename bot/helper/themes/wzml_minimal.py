81a644df8cef8ff0e9c0cbe7930788a5101929d5
LazaaR <Githeroku@outlook.com>
2025-01-07 23:01:18 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -177,7 +177,7 @@ class WZMLStyle:
     STATUS_NAME =       '<b><i>{Name}</i></b>'
 
     #####---------PROGRESSIVE STATUS-------
-    STATUS =            '\n┎ <b><a href="{Url}">{Status}</a></b>'
+    STATUS =            '\n┎ <b><a href="{Url}">{Status}</a></b> {Pertg}'
     BAR =               '\n┃ {Bar}'
     PROCESSED =         '\n┠ <code>{Processed}</code>'
     ETA =               '\n┠ <b>ETA:</b> <code>{Eta}</code>'
